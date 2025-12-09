e3ceec06dcf54f0ebedd36793e8e2125989c140f
magnumbot <n01750770@humber.ca>
2025-12-08 21:08:37 -0500

    Final with Fr/Eng Translation

--- Imageslider_content/imageslider.js
+++ Imageslider_content/imageslider.js
@@ -1,11 +1,14 @@
 const slider = document.getElementById("Images");
+//Variable that starts from the first slide
 let index = 0;
 
 function move(step) {
   const total = slider.children.length;
   index = (index + step + total) % total;
+  // does a sliding animation after you click the arrow
   slider.style.transform = `translateX(-${index * 100}%)`;
 }
-
+//Goes to the next 
 document.getElementById("next").onclick = () => move(1);
+//Goes to the previous 
 document.getElementById("prev").onclick = () => move(-1);
